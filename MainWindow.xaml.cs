568f601dce7a78e7e5d02597689e9dce4be4b382
Adrien Mousty <moustyadrien@hotmail.com>
2018-02-17 22:20:43 +0100

    Bug fixes
    - Improve structure
    - Removed # in patch name

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,24 +24,39 @@ namespace PatchBuilder
                 "Please, fill the description.",
                 "Path specified already existing.",
                 "Unable to browse this folder.",
+                "Please, browse a folder before building."
         };
         private OpenFileDialog openFileDialog1;
         private FolderBrowserDialog folderBrowserDialog1;
 
+        public string folderDirectory { get; set; }
+        public string patchNameWithoutSharp { get; set; }
+        public int errorStatus { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            initValues();
             txtB_patchName.IsReadOnly = true;
+        }
+
+        private void initValues()
+        {
+            folderDirectory = "";
+            patchNameWithoutSharp = ""; //folderDirectory.Replace('#', '');
+            errorStatus = 1;
         }
 
         private void btn_build_Click(object sender, RoutedEventArgs e)
         {
-            int checkEverythingOk = everythingIsChecked();
-            if (checkEverythingOk > 0)
+            errorStatus = everythingIsChecked();
+            if (errorStatus > 0)
             {
                 // Everything is ok ! We could build the result into the patch name.
                 txtB_patchName.IsReadOnly = false;
                 txtB_patchName.Text = txtB_IssueID.Text + "[" + txtB_ticketRef.Text + "] " + txtB_description.Text;
+                patchNameWithoutSharp = txtB_patchName.Text.Replace("#", "");
+                folderDirectory = txtB_browseDirectory.Text;
 
                 // Build launched
                 buildFolder();
@@ -51,15 +66,13 @@ namespace PatchBuilder
                 // Display error message. 
 
                 // Clean description.
-                txtB_patchName.Text = "Error code : " + checkEverythingOk + ".\n" + ErrorMessage[checkEverythingOk * -1];
+                txtB_patchName.Text = "Error code : " + errorStatus + ".\n" + ErrorMessage[errorStatus * -1];
                 txtB_patchName.IsReadOnly = true;
             }
         }
 
         private int everythingIsChecked()
         {
-            int errorCode = 1;
-
             // Verify at least one checkbox is thicked
             if (chkb_Web.IsChecked.Value ||
                 chkb_MVC.IsChecked.Value ||
@@ -75,33 +88,37 @@ namespace PatchBuilder
                         // Check if the issueid is correct
                         if (!txtB_description.Text.Equals(""))
                         {
-
+                            if (txtB_browseDirectory.Text.Equals(""))
+                            {
+                                // No folder specified
+                                errorStatus = -7;
+                            }
                         }
                         else
                         {
                             // Description is empty
-                            errorCode = -4;
+                            errorStatus = -4;
                         }
                     }
                     else
                     {
                         // Ticket ref is incorrect
-                        errorCode = -3;
+                        errorStatus = -3;
                     }
                 }
                 else
                 {
                     // Issue ID is incorrect.
-                    errorCode = -2;
+                    errorStatus = -2;
                 }
             }
             else
             {
                 // Check at least one checkbox
-                errorCode = -1;
+                errorStatus = -1;
             }
 
-            return errorCode;
+            return errorStatus;
         }
 
         private bool checkIssueID(string val)
@@ -177,7 +194,7 @@ namespace PatchBuilder
 
         private int buildFolder()
         {
-            string newDirectory = removeChar(txtB_browseDirectory.Text, new char[] { '#' }) + "/" + txtB_patchName.Text + "/";
+            string newDirectory = folderDirectory + "/" + patchNameWithoutSharp + "/";
             try
             {
                 // We need to iterate for each type selected.
@@ -186,7 +203,7 @@ namespace PatchBuilder
                 if (Directory.Exists(newDirectory))
                 {
                     // Path already existing. 
-                    return -5;
+                    errorStatus = - 5;
                 }
 
                 // Try to create the directory.
@@ -197,14 +214,15 @@ namespace PatchBuilder
             }
             catch (Exception)
             {
-                return -6; // Console.WriteLine("The process failed: {0}", e.ToString());
+                errorStatus = - 6; // Console.WriteLine("The process failed: {0}", e.ToString());
             }
+            return errorStatus;
         }
 
         private int buildSubfolder()
         {
-            string newDirectory = removeChar(txtB_browseDirectory.Text, new char[] { '#' }) + "/" + txtB_patchName.Text + "/";
-            int returnValue = 0;
+            string newDirectory = folderDirectory + "/" + patchNameWithoutSharp + "/";
+            errorStatus = 1;
 
             if (chkb_MVC.IsChecked == true)
             {
@@ -227,17 +245,7 @@ namespace PatchBuilder
                 DirectoryInfo di = Directory.CreateDirectory(newDirectory + "/WEB");
             }
 
-            return returnValue;
-        }
-
-        private string removeChar(string str, char[] charsToRemove)
-        {
-            foreach (var c in charsToRemove)
-            {
-                str = str.Replace(c, '\0');
-            }
-
-            return str;
+            return errorStatus;
         }
     }
 }
